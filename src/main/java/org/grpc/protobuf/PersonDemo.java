2dafaeddf5b09f3131593e86ffcd973f0dd68471
Krishanu Chakraborty <krishanuc1001@users.noreply.github.com>
2023-10-27 14:24:56 +0530

    Create PersonDemo class to add Person instances using newBuilder() and compare them using equals

---
+++ src/main/java/org/grpc/protobuf/PersonDemo.java
@@ -0,0 +1,22 @@
+package org.grpc.protobuf;
+
+import com.proto.generatedProtoClasses.Person;
+
+public class PersonDemo {
+
+    public static void main(String[] args) {
+
+        Person tarzan = Person.newBuilder()
+                .setName("John")
+                .setAge(40)
+                .build();
+
+        Person jane = Person.newBuilder()
+                .setName("Jane")
+                .setAge(30)
+                .build();
+
+        System.out.println(tarzan.equals(jane));
+
+    }
+}
